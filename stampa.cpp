c2820efa2e2766b0329114dbc0bdd236c384444b
Tommaso Pizzutilo <tommaso.pizzutilo@gmail.com>
2024-05-14 10:35:47 +0200

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+Tommaso Pizzutilo 14/05/2024
 */
 
 # include <iostream>
